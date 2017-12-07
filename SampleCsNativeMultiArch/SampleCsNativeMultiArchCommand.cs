e8ee05c219e52d35d8e23d65e8a461e1ca9028d0
Will Pearson <pearswj@gmail.com>
2017-12-07 13:23:22 +0000

    Initial commit

---
+++ SampleCsNativeMultiArch/SampleCsNativeMultiArchCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace SampleCsNativeMultiArch
+{
+  public class SampleCsNativeMultiArchCommand : Rhino.Commands.Command
+  {
+    public override string EnglishName
+    {
+      get { return "SampleCsNativeMultiArchCommand"; }
+    }
+
+    protected override Result RunCommand(Rhino.RhinoDoc doc, RunMode mode)
+    {
+      IntPtr version;
+      try
+      {
+        version = UnsafeNativeMethods.curl_version();
+      }
+      catch (Exception e)
+      {
+        RhinoApp.WriteLine("ERROR: " + e.Message);
+        RhinoApp.WriteLine("Couldn't load libcurl");
+        return Result.Failure;
+      }
+
+      RhinoApp.WriteLine("Successfully loaded libcurl, version: " + version.ToString());
+      return Result.Success;
+    }
+  }
+}
